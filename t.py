2e3364bfbb47464d70686d127e57111fa9013e95
$!#e@L%1108* <devendra.pal@tcgdigital.com>
2022-08-27 04:32:06 +0530

    setupdvc

---
+++ t.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+ 
+# read a titanic.csv file
+# from seaborn library
+df = sns.load_dataset('titanic')
+ 
+# who v/s fare barplot
+sns.barplot(x = 'who',
+            y = 'fare',
+            data = df)
+ 
+# Show the plot
+plt.show()
